9f2ff322f3898832fe663c2f60d6562a0a7ce434
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2023-05-04 10:12:03 -0600

    Game engine physics start

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,30 @@
 #include "Graphics.h"
 #include "Random.h"
 #include <iostream>
+#include "Test.h"
 #include <SDL.h>
 #include <glm/glm.hpp>
 
 int main(int argc, char* argv[]) {
 
-	Graphics graphics;
-	graphics.CreateWindow("Physics", 800, 600);
-	while (1)
-	{
-		graphics.Clear();
-		for (int i = 0; i < 200; i++)
-		{
-			graphics.DrawPoint({ randomf(800),randomf(600) }, { randomf(),randomf(),randomf(),randomf() });
-			graphics.DrawCircle({ randomf(800), randomf(600) }, randomf(10), { randomf(),randomf(),randomf(),randomf() });
-			graphics.DrawFilledCircle({ randomf(800), randomf(600) }, randomf(10), { randomf(),randomf(),randomf(),randomf() });
-			graphics.DrawLine({ randomf(800), randomf(600) }, { randomf(800), randomf(600) }, { randomf(),randomf(),randomf(),randomf() });
-		}
-		graphics.Present();
-	}
+	//Graphics graphics;
+	//graphics.CreateWindow("Physics", 800, 600);
+	//while (1)
+	//{
+	//	graphics.Clear();
+	//	for (int i = 0; i < 200; i++)
+	//	{
+	//		graphics.DrawPoint({ randomf(800),randomf(600) }, { randomf(),randomf(),randomf(),randomf() });
+	//		graphics.DrawCircle({ randomf(800), randomf(600) }, randomf(10), { randomf(),randomf(),randomf(),randomf() });
+	//		graphics.DrawFilledCircle({ randomf(800), randomf(600) }, randomf(10), { randomf(),randomf(),randomf(),randomf() });
+	//		graphics.DrawLine({ randomf(800), randomf(600) }, { randomf(800), randomf(600) }, { randomf(),randomf(),randomf(),randomf() });
+	//	}
+	//	graphics.Present();
+	//}
 
+	Test* test = new Test();
+	test->Initialize();
+	
 
 	//// Run the main loop THIS IS FOR MANUAL EXIT
 	//bool quit = false;
